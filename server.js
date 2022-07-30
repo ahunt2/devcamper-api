f82d996b6b8b29e276ca9c47f97cd48878f42b84
Andrew Hunt <andrew.hunt.5309@gmail.com>
2022-07-30 14:53:07 -0500

    initial api setup

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
